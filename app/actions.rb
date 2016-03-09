681ce8d3c4868e41d481d0b94147b47dba571ee6
RealMagoj <RealMagoj@users.noreply.github.com>
2016-03-09 18:04:09 -0500

    add List/New/Delete functions

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all
+  content_type :json
+  @contacts.to_json
+end
+
+post '/contacts/new' do
+  @contact = Contact.new(
+    name: params[:name],
+    email: params[:email]
+  )
+  if @contact.save
+    puts 'Check'
+    return {'created': true}.to_json
+  end
+end
+
+delete '/contacts/delete' do
+  @contact = Contact.find(params[:id])
+  @contact.destroy
+end
+
+get '/contacts/show/:id' do
+  @contact = Contact.find(params[:id])
+  @contact.to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,57 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('.new').on('click', function() {
+    $.ajax({
+      type: 'post',
+      url: '/contacts/new',
+      dataType: 'json',
+      data: {name: $('#name').val(), email: $('#email').val()},
+      success: function(data){
+        alert('Created new contact!')
+      }
+    });
+  });
+
+  $(".list").on('click', function(){
+    $.ajax({
+      type: 'get',
+      url: '/contacts',
+      dataType: 'json',
+      success: function(data){
+        $('.contact-list').html("");
+        $.each(data, function(index, contact){
+          $('.contact-list').append("<p>" + contact.name + ", " + contact.email + " <button class='delete' id='" + contact.id + "'>Delete</button></p>");
+        });
+      }
+    });
+  });
+
+  $('.contact-list').on('click', '.delete', function(){
+    var contact = $(this).attr('id');
+    console.log(contact);
+    $.ajax({
+      type: 'delete',
+      url: '/contacts/delete',
+      data: { id: contact },
+      success: function() {
+        $('#' + contact).parent().remove();
+      }
+    });
+  });
+
+  $('.show').on('click', function() {
+    $.ajax({
+      type: 'get',
+      url: '/contacts/show/' + $('#id').val(),
+      dataType: 'json',
+      success: function(data){
+        console.log(data)
+        $('.contact-list').empty();
+        $('.contact-list').append("<p>" + data.name + ", " + data.email + " <button class='delete' id='" + data.id + "'>Delete</button></p>");
+      }
+    });
+  });
+
 });
